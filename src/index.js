1625ec3eefe349c2a17ecfa7f7d58ab91b39210f
Andy Lindemann <andy@Andys-MBP.fritz.box>
2017-04-11 18:06:03 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as I18n } from './lib/I18n';
 export { default as Translate } from './lib/Translate';
 export { default as Localize } from './lib/Localize';
-export { default as TranslateInput } from './lib/TranslateInput';
+// export { default as TranslateInput } from './lib/TranslateInput';
